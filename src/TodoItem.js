53a1901e630b2faa6933dedf30e3ed86755c159a
Colin Richardson <richardsonjcolin@gmail.com>
2020-04-23 11:42:22 -0700

    initial commit

---
+++ src/TodoItem.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+function TodoItem() {
+  return (
+    <div></div>
+  );
+}
+
+export default TodoItem;
---
+++ src/TodoList.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import TodoItem from "./TodoItem";
+
+function TodoList(props) {
+  return (
+    <div></div>
+  );
+}
+
+export default TodoList;

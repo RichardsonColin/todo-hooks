70f13ffabcbd6741d83ecf484ceb530885b52000
Colin Richardson <richardsonjcolin@gmail.com>
2020-04-23 11:26:55 -0700

    build initial layout

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
 import './TodoApp.css';
 
 function TodoApp() {
+  // Layout taken from material.ui docs example
   return (
-    <div>
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa"
+      }}
+      elevation={0}
+    >
+      <AppBar color='primary' position='static' style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color='inheirt'>TODOS (WITH HOOKS)</Typography>
+        </Toolbar>
+      </AppBar>
 
-    </div>
+    </Paper>
   );
 }
 

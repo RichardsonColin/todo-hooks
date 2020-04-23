8b8a1604711f759136ed8212699ae580e5f775c0
Colin Richardson <richardsonjcolin@gmail.com>
2020-04-23 12:35:52 -0700

    add form and handle submit

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -2,10 +2,20 @@ import React, { useState } from "react";
 import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
 
-function TodoForm(props) {
+import useInputState from "./hooks/useInputState";
+
+function TodoForm({ addTodo }) {
+  const [value, handleChange, clear] = useInputState("");
+  const submitForm = evt => {
+    evt.preventDefault();
+    addTodo(value);
+    clear();
+  }
   return (
     <Paper>
-      <TextField />
+      <form onSubmit={submitForm}>
+        <TextField value={value} onChange={handleChange} />
+      </form>
     </Paper>
   );
 }
